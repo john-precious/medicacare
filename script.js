a7ab90e504f20c67732a0e4576fc2f2dbe5766d2
Precious john <preciousjohnemeka2022@gmail.com>
2025-07-30 12:22:38 +0100

    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+AOS.init();
+
+const typed = new Typed('.typing-text', {
+  strings: ["Healing starts here.", "Compassion. Care. Confidence.", "Your health, our priority."],
+  typeSpeed: 60,
+  backSpeed: 30,
+  loop: true,
+});
